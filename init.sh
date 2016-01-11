821270b5e140846d40a0d0c63386782782877852
Olivier Lalonde <olalonde@gmail.com>
2016-01-11 10:23:42 -0800

    fix(init): fix init

--- init.sh
+++ init.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
-PROJECT=${PROJECT:-"newproject"}
-
-git clone https://github.com/olalonde/es6-npm-package-scaffold.git "$PROJECT" && \
-  cd "$PROJECT" && \
-  rm -rf .git && \
-  git init && \
-  rm CHANGELOG.md && \
-  echo "# ${PROJECT}" > README.md && \
-  npm init && \
-  npm install
+rm -rf .git && \
+git init && \
+rm CHANGELOG.md && \
+echo "# ${PROJECT}" > README.md && \
+npm init && \
+npm install && \
+rm init.sh
