3606ca96a6db15f0c9f7270eaa511d14d151569b
Olivier Lalonde <olalonde@gmail.com>
2016-01-14 09:46:58 -0800

    fix(misc): small fixes

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ PROJECT="basename $(pwd)"
 rm -rf .git && \
 git init && \
 rm CHANGELOG.md && \
-echo "# ${PROJECT}\n\n# Install\n\n# Usage" > README.md && \
+echo "# ${PROJECT}\n\n## Install\n\n## Usage" > README.md && \
 npm init && \
 npm install && \
 rm init.sh
